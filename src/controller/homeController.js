808d685026cb762d0fe25554a9bc584c05d9ac08
circlerow <hoangthanhhy02@gmail.com>
2023-06-01 01:30:22 +0700

    1/6

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,5 +1,22 @@
+import connection from "../config/connectDB";
+
+
 let getHomepage = (req, res) => {
-    res.render('index.ejs');
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users`',
+        function (err, results, fields) {
+            console.log('Nice');
+            console.log(results); // results contains rows returned by server
+            data = results.map((row) => {
+                return row;
+            }
+            )
+            console.log('check data', JSON.stringify(data));
+            res.render('index.ejs', { dataUser: JSON.stringify(data) });
+        }
+    );
+
 }
 
 module.exports = {
