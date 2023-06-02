81c925b45316facb0b2112d500d0b7a04c39a325
circlerow <hoangthanhhy02@gmail.com>
2023-06-03 02:58:29 +0700

    #16 3/6

--- src/server.js
+++ src/server.js
@@ -4,10 +4,18 @@ import initWebRoute from './route/web'
 import initAPIRoute from './route/api'
 // import connection from './config/connectDB'
 require('dotenv').config()
+var morgan = require('morgan')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+    console.log('Hello World!')
+    next()
+}
+)
+
+app.use(morgan('combined'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -16,6 +24,10 @@ configViewEngine(app);
 //init web routes
 initWebRoute(app);
 initAPIRoute(app);
+
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
